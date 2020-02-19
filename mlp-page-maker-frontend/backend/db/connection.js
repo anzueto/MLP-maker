c93f21c9085c411484e57964de22d616840d511d
anzueto <jose@C4-1152.local>
2020-02-18 23:51:56 -0500

    mongoose connection

---
+++ mlp-page-maker-frontend/backend/db/connection.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/vehicles'
+
+mongoose.connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: true
+});
+
+
+mongoose.Promise = Promise;
+
+module.exports = mongoose;
+
