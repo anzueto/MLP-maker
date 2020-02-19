b5ee16be2784cdcde9791ebf4cd973b728dd590b
anzueto <jose@C4-1152.local>
2020-02-18 23:51:34 -0500

    added index route, show route, create route

---
+++ mlp-page-maker-frontend/backend/controllers/vehicles.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const router = express.Router();
+
+const Vehicle = require('../models/vehicles-schema.js');
+
+// index route
+router.get('/', (req, res) => {
+    Vehicle.find({}).then(vehicles => {
+        res.render('vehicles/Index', { vehicles });
+    });
+});
+
+//Show Route
+router.get('/:id', (req, res) => {
+    Vehicle.findOne({ _id: req.params.id }).then(vehicles => {
+        res.render('vehicles/Show', vehicles);
+    });
+});
+
+// Create Route
+router.post('/', (req, res) => {
+    console.log(req.body);
+    Vehicle.create(req.body).then(() => {
+        res.redirect('/');
+    });
+});
+
+
+module.exports = router;
