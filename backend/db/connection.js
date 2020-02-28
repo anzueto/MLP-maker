a97d5316336404825d5b055175437b576373c3b1
anzueto <jose@C4-1152.local>
2020-02-28 13:14:43 -0500

    mongoose connection message added

--- backend/db/connection.js
+++ backend/db/connection.js
@@ -2,12 +2,16 @@ const mongoose = require('mongoose');
 
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/vehicles'
 
-mongoose.connect(MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: true
-});
+mongoose.connect(MONGODB_URI,
+    {
+        useNewUrlParser: true,
+        useCreateIndex: true
+    });
 
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log('MongoDb database connection established successfully');
+})
 
 mongoose.Promise = Promise;
 
