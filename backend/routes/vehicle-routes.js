665001a7dbbb1079fa3bda4da710f69ca9fe3a3e
anzueto <jose@C4-1152.local>
2020-02-28 13:13:36 -0500

    added delete route and tested

--- backend/routes/vehicle-routes.js
+++ backend/routes/vehicle-routes.js
@@ -7,3 +7,13 @@ router.route('/').get((req, res) => {
         .then(vehicles => res.json(vehicles)) // return all vehicles in json format
         .catch(err => res.status(400).json('Error:' + err)); // if error let me know
 });
+
+// delete route
+router.route('/:id').delete((req, res) => {
+    Vehicle.findByIdAndDelete(req.params.id) //finding by id and delete from database
+        .then(() => res.json('vehicle deleted')) // return delete confirmation
+        .catch(err => res.status(400).json('Error:' + err)); // if errror send us a notification
+});
+
+
+module.exports = router;
