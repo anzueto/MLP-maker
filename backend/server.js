dfd75e97be171088fe243c2738a0d0aae81d6a6b
anzueto <jose@C4-1152.local>
2020-02-28 13:15:10 -0500

    server cleaned

--- backend/server.js
+++ backend/server.js
@@ -1,36 +1,21 @@
 const express = require('express');
+const cors = require('cors');
 const method = require('method-override');
-const app = express();
-const port = process.env.PORT || 3000;
+const app = express(); // create express server
+const port = process.env.PORT || 5000;
+const path = require('path');
+
+require('dotenv').config();
 
 // this will alow us to connect to mongodb database
 const mongoose = require('./db/connection.js');
 
-// middleware
-app.set('view engine', 'jsx');
-app.engine('jsx', require('express-react-views').createEngine());
-app.use(express.urlencoded({ extended: true }));
-app.use(method('_method'));
-app.use(express.static('public'));
+// midleware
+app.use(cors());
+app.use(express.json()); // allow us to parse json
 
-
-const Vehicles = require('./models/vehicles-schema');
-const vehiclesController = require('./controllers/vehicles.js');
-
-// gets the info to the home page
-app.get('/', (req, res) => {
-    Vehicles.find({})
-        .then(vehicles => {
-            res.render('Index', { vehicles });
-        });
-});
-
-
-app.use('/vehicles', vehiclesController);
-
-mongoose.connection.once('open', () => {
-    console.log('connected to mongo');
-});
+const vehiclesRouter = require('./routes/vehicle-routes');
+app.use('/vehicles', vehiclesRouter); // loads vehicles router
 
 // listen router
 app.listen(port, () => {
