214eaa20d63bf5589641694633614cdf6cb7b5f7
anzueto <jose@C4-1152.local>
2020-03-04 23:59:43 -0500

    Intro style components

--- frontend/src/components/Intro.js
+++ frontend/src/components/Intro.js
@@ -1,23 +1,41 @@
 import React from 'react';
 import '../App.scss';
+import styled from 'styled-components';
 
+const Input = styled.input`
+width:70%;
+margin-left:10px;
+`;
+
+const Textarea = styled.textarea`
+width:70%;
+margin-left:10px;
+margin-top:10px;
+`;
+
+const Label = styled.label`
+width:20%;
+font-size:18px;
+`;
 
 class Intro extends React.Component {
     render() {
         return (
             <div className="box">
                 <form action="">
-                    <label htmlFor="title">Title</label>
+
                     <div className="field">
                         <div className="control">
-                            <input className="input" type="text" placeholder="title" />
+                            <Label htmlFor="title">Title</Label>
+                            <Input className="input" type="text" placeholder="title" />
                         </div>
                     </div>
 
-                    <label htmlFor="title">intro</label>
+
                     <div className="field">
                         <div className="control">
-                            <input className="input" type="text" placeholder="intro" />
+                            <Label htmlFor="title">intro</Label>
+                            <Textarea className="textarea is-medium" placeholder="intro content"></Textarea>
                         </div>
                     </div>
                 </form>
